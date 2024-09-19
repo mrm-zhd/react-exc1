a2f2ff73953e40bed4db8ec1608e1e51a9e4157b
mrm-zhd <mmzahedy@gmail.com>
2024-09-19 13:02:19 +0330

    'move-btn'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
-import ShowForm from './components/common/form/form.jsx';
-// import ListUsers from './components/common/users/ListUsers.jsx';
-// import MoveBtn from './components/common/Button/MoveBtn.jsx';
+import MoveBtn from './components/common/Button/MoveBtn.jsx';
 
 function App() {
   return (
     <div className='grid gap-8 items-center justify-center'>
-      {/* <MoveBtn /> */}
-        {/* <ListUsers/>   */}
-        <ShowForm />
+      <MoveBtn />
     </div>
   );
 }
